bf31de14050dd495f001855916c4cb67761f25b0
Zik-Tech <adioisaac24@gmail.com>
2022-11-15 09:07:58 +0100

    Admin and report function modified

--- Day-16/func.py
+++ Day-16/func.py
@@ -1,11 +1,12 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 from time import sleep
 from os import system, name
 
 
-
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
 
 
 class main:
@@ -18,24 +19,24 @@ class main:
 
     def report(self):
         """ Return Resource Balance """
-        MoneyMachine().report()
-        CoffeeMaker().report()
+        money_machine.report()
+        coffee_maker.report()
 
     def checker(self, user_drink):
         """ Check Resources Sufficient"""
         check = Menu().find_drink(user_drink)
-        return CoffeeMaker().is_resource_sufficient(check)
+        return coffee_maker.is_resource_sufficient(check)
 
     def coin(self, user_drink):
         """ Process coins and Check transaction successful """
         cost = Menu().find_drink(user_drink).cost
-        check_pay = MoneyMachine().make_payment(cost)
+        check_pay = money_machine.make_payment(cost)
         return check_pay
 
     def make_coffee(self, user_drink):
         """ Make Coffee """
         drink_request = Menu().find_drink(user_drink)
-        CoffeeMaker().make_coffee(drink_request)
+        coffee_maker.make_coffee(drink_request)
 
     def admin(self):
         admin_name = input("Whats your name: ").lower()
@@ -44,14 +45,15 @@ class main:
             while True:
                 admin_choice = input("What would you like to do (Off/Refill/Report/Quit): ").lower()
                 if admin_choice == "off":
-                    print("Would off in 2 seconds")
+                    print("Would switch off in 2 seconds")
                     return False
                 elif admin_choice == "refill":
-                    CoffeeMaker.resources = {
+                    coffee_maker.resources = {
                         "water": 300,
                         "milk": 200,
                         "coffee": 100,
                     }
+                    print("Refilled")
                     continue
                 elif admin_choice == "report":
                     self.report()
