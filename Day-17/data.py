a7a0d4a3dc4923004211b2a378153bf0e44a4512
Zik-Tech <adioisaac24@gmail.com>
2022-11-16 00:24:26 +0100

    More update to the main and more questions to the data file

--- Day-17/data.py
+++ Day-17/data.py
@@ -1,16 +1,329 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
-             "you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, "
-             "you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+        {
+            "category": "Entertainment: Video Games",
+            "difficulty": "easy",
+            "question": "Peter Molyneux was the founder of Bullfrog Productions.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science & Nature",
+            "difficulty": "medium",
+            "question": "A person can get sunburned on a cloudy day.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science & Nature",
+            "difficulty": "medium",
+            "question": "The Doppler effect applies to light.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "difficulty": "easy",
+            "question": "The main character in the &quot;Half-Life&quot; franchise is named Morgan Freeman.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "General Knowledge",
+            "difficulty": "easy",
+            "question": "The Great Wall of China is visible from the moon.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "General Knowledge",
+            "difficulty": "easy",
+            "question": "Video streaming website YouTube was purchased in it&#039;s"
+                        " entirety by Facebook for US$1.65 billion in stock.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Entertainment: Comics",
+            "difficulty": "hard",
+            "question": "In the comic book &quot;Archie&quot;, Betty is friends with Veronica because she is rich.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Vehicles",
+            "difficulty": "medium",
+            "question": "The Japanese Shinkansen beat the French TGV&#039;s"
+                        " speed record for fastest electric rail train.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Science: Mathematics",
+            "difficulty": "easy",
+            "question": "The &#039;Squaring the Circle&#039; problem is solvable.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Science & Nature",
+            "difficulty": "hard",
+            "question": "It was once believed that injecting shark cartilage into "
+                        "people would prevent them from contracting cancer.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Film",
+            "difficulty": "easy",
+            "question": "In the original script of &quot;The Matrix&quot;, "
+                        "the machines used humans as additional computing power instead of batteries.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "difficulty": "easy",
+            "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Japanese Anime & Manga",
+            "difficulty": "easy",
+            "question": "The anime &quot;Lucky Star&quot; follows the story of one girl who is unaware she is God.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Entertainment: Board Games",
+            "difficulty": "easy",
+            "question": "The card game &quot;Munchkin&quot; won the 2001 Origins Award for Best Traditional Card Game.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "History",
+            "difficulty": "easy",
+            "question": "United States President John F. Kennedy was assassinated during his presidential "
+                        "motorcade in Atlanta, Georgia on November 22nd, 1963.",
+            "correct_answer": "False",
+        },
+        {
+            "category": "Politics",
+            "difficulty": "easy",
+            "question": "Russia passed a law in 2013 which outlaws telling children that homosexuals exist.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "General Knowledge",
+            "difficulty": "medium",
+            "question": "Sitting for more than three hours a day can cut two years off a person&#039;s life expectancy.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science: Computers",
+            "difficulty": "medium",
+            "question": "AMD created the first consumer 64-bit processor.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "General Knowledge",
+            "difficulty": "medium",
+            "question": "The French word to travel is &quot;Travail&quot;",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "difficulty": "hard",
+            "question": "In Undertale, having a &quot;Fun Value&quot; set to 56-57 will play the &quot;Wrong Number "
+                        "Song Call&quot;.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Entertainment: Japanese Anime & Manga",
+            "difficulty": "easy",
+            "question": "Gosho Aoyama Made This Series: (Detective Conan / Case Closed!)",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Geography",
+            "difficulty": "medium",
+            "question": "Antarctica is the largest desert in the world.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "General Knowledge",
+            "difficulty": "easy",
+            "question": "When you cry in space, your tears stick to your face.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science: Computers",
+            "difficulty": "medium",
+            "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science: Gadgets",
+            "difficulty": "easy",
+            "question": "Microphones can be used not only to pick up sound, "
+                        "but also to project sound similar to a speaker.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science: Computers",
+            "difficulty": "easy",
+            "question": "The Windows 7 operating system has six main editions.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "General Knowledge",
+            "difficulty": "easy",
+            "question": "Romanian belongs to the Romance language family, "
+                        "shared with French, Spanish, Portuguese and Italian. ",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Board Games",
+            "difficulty": "easy",
+            "question": "&quot;PAYDAY: The Heist&quot; is a sequel to the board game &quot;Payday&quot;.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Politics",
+            "difficulty": "easy",
+            "question": "One of Barack Obama&#039;s United States presidential "
+                        "campaign slogan&#039;s was &quot;Yes We Can&quot;.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Music",
+            "difficulty": "easy",
+            "question": "Daft Punk originated in France.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Animals",
+            "difficulty": "easy",
+            "question": "The Killer Whale is considered a type of dolphin.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Politics",
+            "difficulty": "medium",
+            "question": "Helen Clark was the 37th Prime Minister of Australia.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Entertainment: Music",
+            "difficulty": "medium",
+            "question": "Nick Mason is the only member to appear on every Pink Floyd album.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "difficulty": "easy",
+            "question": "The ghosts in &quot;Pac-Man&quot; and &quot;Ms. Pac-Man&quot;"
+                        " have completely different behavior.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science & Nature",
+            "difficulty": "medium",
+            "question": "Pneumonoultramicroscopicsilicovolcanoconiosis is a synonym for the disease known as silicosis.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Television",
+            "difficulty": "medium",
+            "question": "An episode of &quot;The Simpsons&quot; is dedicated to Moe Szyslak&#039;s bar rag.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Politics",
+            "difficulty": "easy",
+            "question": "Former president Theodore Roosevelt (1900-1908) ran for "
+                        "another term under the Progressive Party in 1912.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Books",
+            "difficulty": "easy",
+            "question": "&quot;Green Eggs and Ham&quot; consists of only 50 different words.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Film",
+            "difficulty": "medium",
+            "question": "The movie &quot;Tron&quot; received an Oscar nomination for Best Visual Effects.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Entertainment: Music",
+            "difficulty": "easy",
+            "question": "The 2011 movie &quot;The Adventures of Tintin&quot; was directed by Steven Spielberg.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Animals",
+            "difficulty": "easy",
+            "question": "Rabbits can see what&#039;s behind themselves without turning their heads.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science: Mathematics",
+            "difficulty": "easy",
+            "question": "An equilateral triangle always has every angle measuring 60&deg;.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "History",
+            "difficulty": "medium",
+            "question": "The Korean War ended in 1953 without any ceasefire.",
+            "correct_answer": "False",
+
+        },
+        {
+            "category": "Animals",
+            "difficulty": "medium",
+            "question": "You can tell the age of a ladybird by counting the spots on his back.",
+            "correct_answer": "False",
+        },
+        {
+            "category": "General Knowledge",
+            "difficulty": "medium",
+            "question": "Francis Bacon died from a fatal case of pneumonia while "
+                        "he was attempting to preserve meat by stuffing a chicken with snow.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Animals",
+            "difficulty": "easy",
+            "question": "The internet browser Firefox is named after the Red Panda.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "difficulty": "easy",
+            "question": "In Until Dawn, both characters Sam and Mike cannot be "
+                        "killed under any means until the final chapter of the game.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Science & Nature",
+            "difficulty": "medium",
+            "question": "&quot;Tachycardia&quot; or &quot;Tachyarrhythmia&quot; "
+                        "refers to a resting heart-rate near or over 100 BPM.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "difficulty": "hard",
+            "question": "The Paradox Interactive game &quot;Stellaris&quot; was released in 2016.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Geography",
+            "difficulty": "medium",
+            "question": "Is Tartu the capital of Estonia?",
+            "correct_answer": "False",
+        }
+    ]
